708f01909c804488cd62efb313c5074818d1ceb7
Vincent <vincent.renard@gmx.com>
2018-11-09 16:36:14 +0100

    Correction
    Signed-off-by: Vincent <vincent.renard@gmx.com>

--- app/src/main/java/univ/etu/tachemun/db/managers/UtilisateurManager.java
+++ app/src/main/java/univ/etu/tachemun/db/managers/UtilisateurManager.java
@@ -12,23 +12,11 @@ import univ.etu.tachemun.db.tableclass.Utilisateur;
 
 public class UtilisateurManager extends TableManager {
 
-
-
-
-
-    /*
-    `pseudo_Utilisateur` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `dateInscription_Utilisateur` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `motDePasse_Utilisateur` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `mail_Utilisateur` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-  */
-
     static final String tableName = "Utilisateur";
     static final String ID_UTILISATEUR = "pseudo_Utilisateur";
     static final String PASSWORD = "motDePasse_Utilisateur";
     static final String MAIL = "mail_Utilisateur";
     static final String DateHeure_INSCRIPTION = "dateInscription_Utilisateur";
-
 
     public static final String createTableScript = "CREATE TABLE " + tableName +
             " (" + ID_UTILISATEUR + " TEXT PRIMARY KEY NOT NULL,"
@@ -45,7 +33,6 @@ public class UtilisateurManager extends TableManager {
     public UtilisateurManager(Context context) {
         super(context);
     }
-
 
     private static String getSHA256(String input) {
 
@@ -77,8 +64,10 @@ public class UtilisateurManager extends TableManager {
 
     public long insertRaw(Utilisateur u) {
         ContentValues v = putInContent(u);
-
-        return db.insert(tableName, null, v);
+        this.open();
+        long r = db.insert(tableName, null, v);
+        this.close();
+        return r;
     }
 
     public long insertNew(Utilisateur u) {
@@ -86,21 +75,19 @@ public class UtilisateurManager extends TableManager {
         v.remove(PASSWORD);
         v.put(PASSWORD, getSHA256(u.getMotDePasse()));
         this.open();
-        if(db.isOpen()){
-            return db.insert(tableName, null, v);
-        }
-        else{
-            return 0;
-        }
-
+        long r = db.insert(tableName, null, v);
+        this.close();
+        return r;
     }
 
-    public int update(Utilisateur u) {
+    public long update(Utilisateur u) {
         ContentValues v = putInContent(u);
         String where = ID_UTILISATEUR + " = ?";
         String[] whereArgs = {u.getPseudo() + ""};
         this.open();
-        return db.update(tableName, v, where, whereArgs);
+        long r = db.update(tableName, v, where, whereArgs);
+        this.close();
+        return r;
     }
 
     /**
@@ -110,45 +97,46 @@ public class UtilisateurManager extends TableManager {
      * @param u
      * @return
      */
-    public int updatePseudoUser(String ancienPseudo, Utilisateur u) {
-
-
+    public long updatePseudoUser(String ancienPseudo, Utilisateur u) {
         ContentValues values = putInContent(u);
         String where = ID_UTILISATEUR + " = ?";
         String[] whereArgs = {ancienPseudo + ""};
         this.open();
-        return db.update(tableName, values, where, whereArgs);
-    }
-
-
-    public int delete(Utilisateur u) {
+        long r = db.update(tableName, values, where, whereArgs);
         this.close();
-        return delete(u.getPseudo());
+        return r;
     }
 
-    private int delete(String pseudo) {
+    public long delete(Utilisateur u) {
+        this.open();
+        long r = delete(u.getPseudo());
+        this.close();
+        return r;
+    }
+
+    private long delete(String pseudo) {
 
         String where = ID_UTILISATEUR + " = ?";
         String[] whereArgs = {pseudo + ""};
         this.open();
-        return db.delete(tableName, where, whereArgs);
+        long r = db.delete(tableName, where, whereArgs);
+        this.close();
+        return r;
     }
 
-
     public boolean connectUser(String userName, String password) {
-
-
-        Utilisateur u = new Utilisateur();
         this.open();
         Cursor c = db.rawQuery(
-                    "SELECT " + ID_UTILISATEUR + " FROM " + tableName + " WHERE " +
-                            ID_UTILISATEUR + "=\"" + userName + "\" AND " + PASSWORD + "=\"" + getSHA256(password) + "\"", null);
+                "SELECT " + ID_UTILISATEUR + " FROM " + tableName + " WHERE " +
+                        ID_UTILISATEUR + "=\"" + userName + "\" AND " + PASSWORD + "=\"" + getSHA256(password) + "\"", null);
 
-        if (c.moveToFirst()) {
-            this.close();
+        if (c.getCount() != 0) {
             c.close();
+            this.close();
             return true;
         } else {
+
+            c.close();
             this.close();
             return false;
         }
@@ -163,14 +151,18 @@ public class UtilisateurManager extends TableManager {
                 "SELECT " + ID_UTILISATEUR + ", " + PASSWORD + ", " + MAIL + " , " + DateHeure_INSCRIPTION + " FROM " + tableName + " WHERE " +
                         ID_UTILISATEUR + "=" + id, null);
         this.close();
-        if (c.moveToFirst()) {
+        if (c.getCount() != 0) {
             u.setPseudo(c.getString(c.getColumnIndex(ID_UTILISATEUR)));
             u.setMotDePasse(c.getString(c.getColumnIndex(PASSWORD)));
             u.setMail(c.getString(c.getColumnIndex(MAIL)));
             u.setDateInscription(c.getInt(c.getColumnIndex(DateHeure_INSCRIPTION)));
             c.close();
+            this.close();
             return u;
         } else {
+
+            c.close();
+            this.close();
             return null;
         }
 
@@ -178,19 +170,17 @@ public class UtilisateurManager extends TableManager {
 
     public boolean isPseudoInDb(String pseudo) {
         this.open();
-        Cursor c = null;
-        if(db.isOpen()) {
-            c = db.rawQuery(
-                    "SELECT " + ID_UTILISATEUR + " FROM " + tableName + " WHERE " +
-                            ID_UTILISATEUR + "= \"" + pseudo +"\"", null);
-        }
+        Cursor c = db.rawQuery(
+                "SELECT " + ID_UTILISATEUR + " FROM " + tableName + " WHERE " +
+                        ID_UTILISATEUR + "=\"" + pseudo + "\"", null);
 
-        if (c.moveToFirst()) {
+        if (c.getCount() != 0) {
             this.close();
             c.close();
             return true;
         } else {
             this.close();
+            c.close();
             return false;
         }
 
